6389cf6130c3249242bfe4b39ab0265380272a21
Tymon Sawicz <290407@student.pwr.edu.pl>
2026-05-22 10:52:50 +0200

    dodano Need i klasy dziedziczące w podstawowej wersji z diagramu, jedyna zmiana to dodanie threshold w Need zeby IsCritical mialo sens

--- Zoo/Needs/Happiness.cs
+++ Zoo/Needs/Happiness.cs
@@ -2,4 +2,7 @@ namespace Zoo.Needs;
 
 public class Happiness : Need
 {
+   public Happiness(int initialValue) 
+        : base(NeedType.HAPPINESS, initialValue, 100, 35){}
+
 }
--- Zoo/Needs/Thirst.cs
+++ Zoo/Needs/Thirst.cs
@@ -2,4 +2,6 @@ namespace Zoo.Needs;
 
 public class Thirst : Need
 {
+    public Thirst(int initialValue) 
+        : base(NeedType.THIRST, initialValue, 100, 40){}
 }
