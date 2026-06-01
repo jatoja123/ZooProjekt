d795591e9f58e284f293f56694e04baf1c21f16d
Tymon Sawicz <290407@student.pwr.edu.pl>
2026-06-01 11:44:19 +0200

    dostep do AnimalNeeds

--- Zoo/Animals/Animal.cs
+++ Zoo/Animals/Animal.cs
@@ -5,19 +5,20 @@ public abstract class Animal
     public bool IsInHabitat => Habitat != null;
     public LocationHabitat? Habitat = null;
     private string _name;
-    public List <Need> _animalNeeds {get; private set;}
+    public List<Need> AnimalNeeds { get; private set; }
     //private List <EnviromentalNeed> _requiredEnviroment;
     
     public Animal(string name)
     {
         _name = name;
+        AnimalNeeds = [new Hunger(100), new Thirst(100), new Happiness(100)];
     }
     
     public void Update(NotifyEvent gameEvent)
     {
         // if(event == NotifyEvent.TURN_ENDED)
         // {
-        //     foreach(Need need in _animalNeeds)
+        //     foreach(Need need in AnimalNeeds)
         //     {
         //         need.Decrease(10);
         //     }
@@ -26,7 +27,7 @@ public abstract class Animal
         
     public void Feed()
     {
-        Need hunger = _animalNeeds.FirstOrDefault(n => n.Type == NeedType.HUNGER);
+        Need? hunger = AnimalNeeds.FirstOrDefault(n => n.Type == NeedType.HUNGER);
         if (hunger != null)
         {
             hunger.Increase(30);
@@ -34,7 +35,7 @@ public abstract class Animal
     }
     public void GiveWater()
     {
-        Need thirst = _animalNeeds.FirstOrDefault(n => n.Type == NeedType.THIRST);
+        Need? thirst = AnimalNeeds.FirstOrDefault(n => n.Type == NeedType.THIRST);
         
         if (thirst != null)
         {
@@ -43,7 +44,7 @@ public abstract class Animal
     }
     public void Play()
     {
-        Need happiness = _animalNeeds.FirstOrDefault(n => n.Type == NeedType.HAPPINESS);
+        Need? happiness = AnimalNeeds.FirstOrDefault(n => n.Type == NeedType.HAPPINESS);
         
         if (happiness != null)
         {
