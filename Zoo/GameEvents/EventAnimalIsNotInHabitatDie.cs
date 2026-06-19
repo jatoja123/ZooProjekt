508ee768bb8806a00a3086b99da78078c9b343f9
MaciejTroc <290393@student.pwr.edu.pl>
2026-06-19 23:46:40 +0200

    panom podziekujemy

--- Zoo/GameEvents/EventAnimalIsNotInHabitatDie.cs
+++ Zoo/GameEvents/EventAnimalIsNotInHabitatDie.cs
@@ -1,24 +1,24 @@
-using System.Linq;
-using Zoo.Animals;
+// using System.Linq;
+// using Zoo.Animals;
 
-namespace Zoo.GameEvents;
+// namespace Zoo.GameEvents;
 
-public class EventAnimalsNotInHabitatDie : GameEvent
-{
-    public override float EventChance() => 1;
-    public override GameEventType EventType => GameEventType.EndOfTurn;
+// public class EventAnimalsNotInHabitatDie : GameEvent
+// {
+//     public override float EventChance() => 1;
+//     public override GameEventType EventType => GameEventType.EndOfTurn;
     
-    private AnimalsController animalsController => GameController.Instance.AnimalsController;
+//     private AnimalsController animalsController => GameController.Instance.AnimalsController;
     
-    public override void Trigger()
-    {
-        var animalsToDie = animalsController.FreeAnimals.ToList();
-        foreach (var animal in animalsToDie)
-        {
-            string message = $"Zwierze umiera, nie bylo na wybiegu: {animal.Name}";
-            GameController.Instance.ConsoleDisplay.DisplayInfo(message);
-            GameController.Instance.TriggerPopupEvent(message);
-            animalsController.RemoveAnimal(animal);
-        }
-    }
-}
+//     public override void Trigger()
+//     {
+//         var animalsToDie = animalsController.FreeAnimals.ToList();
+//         foreach (var animal in animalsToDie)
+//         {
+//             string message = $"Zwierze umiera, nie bylo na wybiegu: {animal.Name}";
+//             GameController.Instance.ConsoleDisplay.DisplayInfo(message);
+//             GameController.Instance.TriggerPopupEvent(message);
+//             animalsController.RemoveAnimal(animal);
+//         }
+//     }
+// }
